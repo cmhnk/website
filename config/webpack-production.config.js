edabf5305852b23bc45e7a002bcdbe82f8e6c7a9
cmhnk <corinne@automatedinsights.com>
2017-07-09 19:15:25 -0400

    Update production script to see if that solves image issue on Heroku

--- config/webpack-production.config.js
+++ config/webpack-production.config.js
@@ -3,7 +3,8 @@ const devConfig = require('./webpack.config.js');
 
 const config = Object.assign({}, devConfig, {
   entry: [
-    path.join(__dirname, '/../app/src/main.js'),
+    // path.join(__dirname, '/../app/src/main.js'),
+      path.join(__dirname, '/../dist/main.bundle.js'),
   ],
 });
 
