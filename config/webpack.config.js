30acd96749f891dd1b02a3fa92af796d698bcb58
cmhnk <corinne@automatedinsights.com>
2017-07-09 18:38:22 -0400

    Add url-loader package to manage images with webpack

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -32,6 +32,16 @@ module.exports = {
         },
       }],
     }, {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      }, {
       test: /\.scss/,
       use: [
         'style-loader', {
