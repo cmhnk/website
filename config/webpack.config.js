214c32c9df3fa8ff862b4a426eef0b0b8e08e0c2
cmhnk <corinne@automatedinsights.com>
2017-07-09 19:20:25 -0400

    Remove old index file

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
   ],
   output: {
     path: path.join(__dirname, '/../dist/'),
-    filename: '[name].bundle.js',
+    filename: 'bundle.js',
     publicPath: '/',
   },
   plugins: plugins.concat(sharedPlugins),
