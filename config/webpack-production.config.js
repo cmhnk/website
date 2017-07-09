0a20a49a8db4ac8d88d19ffc59ea402b4aff9b7b
cmhnk <corinne@automatedinsights.com>
2017-07-09 19:29:24 -0400

    Run npm run build:development and deploy

--- config/webpack-production.config.js
+++ config/webpack-production.config.js
@@ -4,7 +4,7 @@ const devConfig = require('./webpack.config.js');
 const config = Object.assign({}, devConfig, {
   entry: [
     // path.join(__dirname, '/../app/src/main.js'),
-    path.join(__dirname, '/../dist/main.bundle.js'),
+    path.join(__dirname, '/../dist/bundle.js'),
   ],
 });
 
